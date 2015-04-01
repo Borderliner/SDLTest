5d130c8d58eb96ff42c6207cb1194666b89b0a5c
Katsuyori <katsuyori@outlook.com>
2015-04-01 14:47:42 +0430

    main.cpp change

--- main.cpp
+++ main.cpp
@@ -1,33 +1,18 @@
-#include "SDL.h"
+#include "src/Game.h"
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+Game* game = nullptr;
 
-int main(int argc, char ** argv) {
-  /* Declare window and renderer */
-  SDL_Window * window;
-  SDL_Renderer * renderer;
-
-  /* Initialize SDL */
-  SDL_Init(SDL_INIT_EVERYTHING);
-  window = SDL_CreateWindow("SDLTest",
-                            SDL_WINDOWPOS_CENTERED,
-                            SDL_WINDOWPOS_CENTERED,
-                            WINDOW_WIDTH, WINDOW_HEIGHT,
-			    0);
-  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-  /* Main game loop */
-  while (!SDL_QuitRequested()) {
-    SDL_RenderClear(renderer);
-    /* Place your simulation code and rendering code here */
-    SDL_RenderPresent(renderer);
-  }
-
-  /* Deinitialize everything */
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-
-  return 0;
+int main(int argc, char** argv)
+{
+	game = new Game();
+	game -> init("My Game", 800, 600, SDL_WINDOW_SHOWN);
+	while(game->isRunning())
+	{
+		game -> handleEvents();
+		game -> update();
+		game -> render();
+	}
+	
+	game ->clean();
+	return 0;
 }
