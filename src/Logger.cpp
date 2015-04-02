773cf2aeb3c2c4ff041fd25797943a7954ef2933
Katsuyori <katsuyori@outlook.com>
2015-04-03 02:36:20 +0430

    stampDate() implementation

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -35,3 +35,17 @@ void Logger::error(std::string message)
 	*fileOut<<"ERROR -> "<<message<<std::endl;
 	fileOut->close();
 }
+
+void Logger::stampDate()
+{
+	std::time_t now = std::time(0);
+	std::tm* ltm = std::localtime(&now);
+	fileOut->open("Logger.log", std::ios::out | std::ios::app);
+	*fileOut<<1900 + ltm->tm_year; //Prints out the year
+	*fileOut<<"-"<<1 + ltm->tm_mon; //Prints out the month
+	*fileOut<<"-"<<ltm->tm_mday; //Prints out the day
+	*fileOut<<", "<<1 + ltm->tm_hour; //Prints out the hour
+	*fileOut<<":"<< 1 + ltm->tm_min; //Prints out the minute
+	*fileOut<<":"<< 1 + ltm->tm_sec; //Prints out the seconds
+	*fileOut<<std::endl<<"----------------------------------------"<<std::endl; //Insert 40 dashes
+}
