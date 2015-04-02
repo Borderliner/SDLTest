5c4279290f92e43b9852f553f3b92d971f6dfe08
Katsuyori <katsuyori@outlook.com>
2015-04-03 03:22:51 +0430

    Rearranged folders!

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "Game/Game.h"
+
+
+int main(int argc, char** argv)
+{
+	Game game;
+	game.init("My Game", 800, 600, SDL_WINDOW_SHOWN);
+	while(game.isRunning())
+	{
+		game.handleEvents();
+		game.update();
+		game.render();
+	}
+	
+	game.clean();
+	return 0;
+}
