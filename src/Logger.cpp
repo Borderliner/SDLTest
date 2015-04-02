b1d9d05ebf575f86e23cfa6afbf8eeb4f9bc8172
Katsuyori <katsuyori@outlook.com>
2015-04-03 03:10:56 +0430

    Closed any opening stream before outputting to Logger.log. There was a bug fixed here.

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -14,14 +14,16 @@ Logger::~Logger()
 
 void Logger::warning(std::string message)
 {
-	std::cout<<"WARNING -> "<<message<<std::endl;
+	fileOut->close(); //Close any opening stream
+	std::cout<<"WARNING -> "<<message<<std::endl; //Output in console
 	fileOut->open("Logger.log", std::ios::out | std::ios::app);
-	*fileOut<<"WARNING -> "<<message<<std::endl;
+	*fileOut<<"WARNING -> "<<message<<std::endl; //Output in Logger.log
 	fileOut->close();
 }
 
 void Logger::note(std::string message)
 {
+	fileOut->close();
 	std::cout<<"Note -> "<<message<<std::endl;
 	fileOut->open("Logger.log", std::ios::out | std::ios::app);
 	*fileOut<<"Note -> "<<message<<std::endl;
@@ -30,6 +32,7 @@ void Logger::note(std::string message)
 
 void Logger::error(std::string message)
 {
+	fileOut->close();
 	std::cout<<"ERROR -> "<<message<<std::endl;
 	fileOut->open("Logger.log", std::ios::out | std::ios::app);
 	*fileOut<<"ERROR -> "<<message<<std::endl;
@@ -38,15 +41,17 @@ void Logger::error(std::string message)
 
 void Logger::stampDate()
 {
+	fileOut->close();
 	std::time_t now = std::time(0);
 	std::tm* ltm = std::localtime(&now);
 	fileOut->open("Logger.log", std::ios::out | std::ios::app);
+	*fileOut<<std::endl<<"-----";
 	*fileOut<<1900 + ltm->tm_year; //Prints out the year
 	*fileOut<<"-"<<1 + ltm->tm_mon; //Prints out the month
 	*fileOut<<"-"<<ltm->tm_mday; //Prints out the day
 	*fileOut<<", "<<1 + ltm->tm_hour; //Prints out the hour
 	*fileOut<<":"<< 1 + ltm->tm_min; //Prints out the minute
 	*fileOut<<":"<< 1 + ltm->tm_sec; //Prints out the seconds
-	*fileOut<<std::endl<<"----------------------------------------"<<std::endl; //Insert 40 dashes
+	*fileOut<<"-----------------------------------"<<std::endl; //Insert 35 dashes
 	fileOut->close();
 }
