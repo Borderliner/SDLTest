440fab2b4a4090f7e4567cf29b51604a7a30aba3
Katsuyori <katsuyori@outlook.com>
2015-04-03 02:53:34 +0430

    Created statically allocated Game object instead of a dynamic one.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include "src/Game.h"
 
-Game* game = nullptr;
 
 int main(int argc, char** argv)
 {
-	game = new Game();
-	game -> init("My Game", 800, 600, SDL_WINDOW_SHOWN);
-	while(game->isRunning())
+	Game game();
+	game.init("My Game", 800, 600, SDL_WINDOW_SHOWN);
+	while(game.isRunning())
 	{
-		game -> handleEvents();
-		game -> update();
-		game -> render();
+		game.handleEvents();
+		game.update();
+		game.render();
 	}
 	
-	game ->clean();
+	game.clean();
 	return 0;
 }
